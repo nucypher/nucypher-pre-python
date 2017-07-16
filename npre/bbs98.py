ae80f69a0a1c93ff5ded0879ff9a4e24934d8794
Michael Egorov <michwill@gmail.com>
2017-07-16 16:17:04 -0700

    Init, load and save the encryption object

--- npre/bbs98.py
+++ npre/bbs98.py
@@ -1,11 +1,26 @@
-from npre.elliptic_curve import G, elliptic_curve
-from npre.elliptic_curve import random as group_random
+import npre.elliptic_curve as ec
 from npre import curves
 
 
 class PRE(object):
     def __init__(self, curve=curves.secp256k1, g=None):
         self.curve = curves.secp256k1
-        self.ecgroup = elliptic_curve(nid=self.curve)
+        self.ecgroup = ec.elliptic_curve(nid=self.curve)
         if g is None:
-            self.g = group_random(self.ecgroup, G)  # XXX load??
+            self.g = ec.random(self.ecgroup, ec.G)
+        else:
+            self.g = ec.deserialize(self.ecgroup, g)
+
+    def to_dict(self):
+        return {'g': ec.serialize(self.g),
+                'curve': self.curve}
+
+    def serialize(self):
+        import msgpack
+        return msgpack.dumps(self.to_dict())
+
+    @classmethod
+    def deserialize(cls, s):
+        import msgpack
+        d = msgpack.loads(s)
+        return cls(**{x.decode(): d[x] for x in d})
--- tests/test_bbs98.py
+++ tests/test_bbs98.py
@@ -1,5 +1,15 @@
 from npre import bbs98
 
 
-def test_init():
-    bbs98.PRE()
+def test_serde():
+    pre = bbs98.PRE()
+    pre1 = bbs98.PRE(**pre.to_dict())
+
+    assert pre.g == pre1.g
+    assert pre.curve == pre1.curve
+
+    s = pre.serialize()
+    pre2 = bbs98.PRE.deserialize(s)
+
+    assert pre.g == pre2.g
+    assert pre.curve == pre2.curve
