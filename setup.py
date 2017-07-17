9f2d135a4581744a1d4b9e9008844381471394cc
Michael Egorov <michwill@gmail.com>
2017-07-16 21:15:54 -0700

    Using tox

--- setup.py
+++ setup.py
@@ -5,14 +5,25 @@ from distutils.core import setup, Extension
 
 # Haven't tried that on Windows or Mac
 
+INSTALL_REQUIRES = ['msgpack-python']
+
+TESTS_REQUIRE = [
+    'pytest',
+    'coverage',
+    'pytest-cov',
+    'pdbpp',
+]
+
 elliptic_curve = Extension(
         'npre.elliptic_curve',
-        sources=['npre/elliptic_curve/ecmodule.c', 'npre/util/base64.c'],
-        include_dirs=['npre/util'],
+        sources=['npre/elliptic_curve/ecmodule.c', 'npre/util/base64/base64.c'],
+        include_dirs=['npre/base64/util', 'npre/elliptic_curve'],
         libraries=['crypto', 'gmp'])
 
 setup(name='npre',
       version='0.1',
       description='NuCypher proxy re-encryption libraries',
       ext_modules=[elliptic_curve],
-      packages=['npre'])
+      extras_require={'testing': TESTS_REQUIRE},
+      install_requires=INSTALL_REQUIRES,
+      packages=['npre', 'npre.util'])
