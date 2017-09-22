c5aeab9b41c12d54c2a21bc804dd2bb9b10139e6
Michael Egorov <michwill@gmail.com>
2017-09-21 22:54:04 -0400

    Test reencryption

--- tests/test_umbral.py
+++ tests/test_umbral.py
@@ -9,5 +9,26 @@ def test_encrypt_decrypt():
     sym_key, ekey = pre.encapsulate(pub_key)
     assert len(sym_key) == 32
 
+    # The symmetric key sym_key should be used for block cipher
+
     sym_key_2 = pre.decapsulate(priv_key, ekey)
     assert sym_key_2 == sym_key
+
+
+def test_reencrypt():
+    pre = umbral.PRE()
+    priv_alice = pre.gen_priv()
+    pub_alice = pre.priv2pub(priv_alice)
+    priv_bob = pre.gen_priv()
+
+    sym_key, ekey_alice = pre.encapsulate(pub_alice)
+
+    rk_ab = pre.rekey(priv_alice, priv_bob)
+
+    ekey_bob = pre.reencrypt(rk_ab, ekey_alice)
+
+    sym_key_2 = pre.decapsulate(priv_bob, ekey_bob)
+    assert sym_key_2 == sym_key
+
+    sym_key_3 = pre.decapsulate(priv_bob, ekey_alice)
+    assert sym_key_3 != sym_key
